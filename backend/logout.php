0abc74f6231cfce8d477bace1a628763903d2ddc
mrsaif888 <105549569+mrsaif888@users.noreply.github.com>
2025-04-06 21:11:04 -0700

    Change the UI of all dashboards

--- backend/logout.php
+++ backend/logout.php
@@ -3,6 +3,6 @@ session_start();
 session_unset();  // Clear all session variables
 session_destroy(); // Destroy the session
 
-header("Location: ../admin/login.html");
+header("Location: ../index.html");
 exit();
 ?>
